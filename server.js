827f4db8b0c9ad11fddce1967f8921e210a53a43
OskarBun <oskarb@oskarmac.local>
2017-05-23 11:53:03 +0100

    port and cleared

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.ws.use(async ctx => {
 })
 
 //Run app
-app.listen(8080, () => {
-    console.log("Listening on 8080")
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log("Listening on "+port)
 })
